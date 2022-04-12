8b2bfa58135873ab1ceb814197c4ef6f14c34d9d
Sophie Verger <sophie@kernl.fr>
2022-04-12 16:40:21 +0200

    add vue js calculator css

--- calculatrice-vue-js/src/components/Calculator.vue
+++ calculatrice-vue-js/src/components/Calculator.vue
@@ -1,30 +1,30 @@
 
 <template>
 	<div class="calculator">
-		<div>
-			<button v-on:click="getValue(0)">0</button>
-			<button v-on:click="getValue(1)" data-id="1">1</button>
-			<button v-on:click="getValue(2)" data-id="2">2</button>
-			<button v-on:click="getValue(3)" data-id="3">3</button>
-			<button v-on:click="getValue(4)" data-id="4">4</button>
-			<button v-on:click="getValue(5)" data-id="5">5</button>
-			<button v-on:click="getValue(6)" data-id="6">6</button>
-			<button v-on:click="getValue(7)" data-id="7">7</button>
-			<button v-on:click="getValue(8)" data-id="8">8</button>
-			<button v-on:click="getValue(9)" data-id="9">9</button>    
+		<div class="total">{{ total || 0 }}</div>
+		<div class="top-buttons">
+			<button v-on:click="clear()">AC</button>
+			<button v-on:click="percent()">%</button>
+			<button v-on:click="equal()">=</button>
 		</div>
-		<div>
+		<div class="operators">
 			<button v-on:click="add()">+</button>
 			<button v-on:click="substract()">-</button>
 			<button v-on:click="divide()">/</button>
 			<button v-on:click="multiplicate()">*</button>    
 		</div>
-		<div>
-			<button v-on:click="clear()">AC</button>
-			<button v-on:click="percent()">%</button>
-			<button v-on:click="equal()">=</button>
+		<div class="numbers">
+			<button v-on:click="getValue(9)" data-id="9">9</button>    
+			<button v-on:click="getValue(8)" data-id="8">8</button>
+			<button v-on:click="getValue(7)" data-id="7">7</button>
+			<button v-on:click="getValue(6)" data-id="6">6</button>
+			<button v-on:click="getValue(5)" data-id="5">5</button>
+			<button v-on:click="getValue(4)" data-id="4">4</button>
+			<button v-on:click="getValue(3)" data-id="3">3</button>
+			<button v-on:click="getValue(2)" data-id="2">2</button>
+			<button v-on:click="getValue(1)" data-id="1">1</button>
+			<button v-on:click="getValue(0)">0</button>
 		</div>
-		<div>{{ total || 0 }}</div>
 	</div>
 </template>
 
@@ -83,4 +83,43 @@
 </script>
 
 <style scoped>
+.calculator {
+	width: 400px;
+	display: flex;
+	flex-direction: column;
+}
+
+button {
+	height: 50px;
+	font-weight: bold;
+	font-size: 20px;
+}
+
+.total {
+	background-color: greenyellow;
+	width: 100%;
+	height: 50px;
+	font-size: 30px;
+	border: 1px solid black;
+	text-align: center;
+	padding-top: 3%;
+}
+
+.operators button {
+	width: 25%;
+	background-color: lightseagreen;
+}
+
+.top-buttons button {
+	background-color: lightcyan;
+	width: 33.333%;
+}
+
+.numbers button {
+	width: 33.333%;
+}
+
+.numbers button:last-child {
+	width: 100%;
+}
 </style>
